445d7f78b1931d8244bb962f06bb92b7f13f6b70
Alexander Kim <alkim@appulate.com>
2024-02-06 19:27:04 +0500

    334. Increasing Triplet Subsequence

---
+++ Medium/IncreasingTripletSubsequence/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool IncreasingTriplet(int[] nums)
+    {
+        int minVal = int.MaxValue; 
+        int maxVal = int.MaxValue;
+
+        foreach (int n in nums)
+        {
+            if (n <= minVal)
+                minVal = n;
+            else if (n <= maxVal)
+                maxVal = n;
+            else
+                return true;
+        }
+
+        return false;
+    
+    }
+}
---
+++ Medium/IncreasingTripletSubsequence/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace ProductOfArrayExceptSelf;
+
+[TestFixture]
+[Parallelizable]
+public class UnitTest
+{
+    private static IEnumerable<TestCaseData> Cases()
+    {
+        yield return new TestCaseData(new object[] {new[] {1, 2, 3, 4, 5}}).Returns(true);
+        yield return new TestCaseData(new object[] {new[] {5, 4, 3, 2, 1}}).Returns(false);
+        yield return new TestCaseData(new object[] {new[] {2, 1, 5, 0, 4, 6}}).Returns(true);
+        yield return new TestCaseData(new object[] {new[] {20, 100, 10, 12, 5, 13}}).Returns(true);
+        yield return new TestCaseData(new object[] {new[] {1, 2, 1, 3}}).Returns(true);
+        yield return new TestCaseData(new object[] {new[] {1, 2, 2147483647}}).Returns(true);
+        yield return new TestCaseData(new object[] {new[] {6, 7, 1, 2}}).Returns(false);
+    }
+
+    [TestCaseSource(nameof(Cases))]
+    public bool ReturnsArrayEqualOfAllElements(int[] nums)
+    {
+        return new Solution().IncreasingTriplet(nums);
+    }
+}
