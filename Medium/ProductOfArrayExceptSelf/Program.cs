66d8a866154291d840186a8599dab61b0702fcc1
Alexander Kim <alkim@appulate.com>
2024-02-06 17:30:03 +0500

    238. Product of Array Except Self

---
+++ Medium/ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        int temp = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = temp;
+            temp *= nums[i];
+        }
+
+        temp = nums[^1];
+        for (int i = result.Length - 2; i > 0; i--)
+        {
+            result[i] *= temp;
+            temp *= nums[i];
+        }
+
+        result[0] = temp;
+        return result;
+    }
+}
---
+++ Medium/ProductOfArrayExceptSelf/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace ProductOfArrayExceptSelf;
+
+[TestFixture]
+[Parallelizable]
+public class UnitTest
+{
+    private static IEnumerable<TestCaseData> Cases()
+    {
+        yield return new TestCaseData(new object[] {new[] {2, 1, 3, 2, 1}}).Returns(new[] {6, 12, 4, 6, 12});
+        yield return new TestCaseData(new object[] {new[] {1, 2, 3, 4}}).Returns(new[] {24, 12, 8, 6});
+        yield return new TestCaseData(new object[] {new[] {-1, 1, 0, -3, 3}}).Returns(new[] {0, 0, 9, 0, 0});
+    }
+
+    [TestCaseSource(nameof(Cases))]
+    public int[] ReturnsArrayEqualOfAllElements(int[] nums)
+    {
+        return new Solution().ProductExceptSelf(nums);
+    }
+}
