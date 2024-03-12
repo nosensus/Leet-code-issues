1c1a0d3f9da66787a1dd0e5f1ff1699dfd3a0263
Alexander Kim <alkim@appulate.com>
2024-03-12 18:33:31 +0500

    feat: 13. Roman to Integer - faster solution

--- Easy/RomanToInteger/Program.cs
+++ Easy/RomanToInteger/Program.cs
@@ -6,67 +6,100 @@
 
 public class Solution
 {
+    readonly Dictionary<char, int> dictionary = new()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
     public int RomanToInt(string s)
     {
         int convertedInteger = 0;
-        var romanToInt = new Dictionary<string, int>()
-        {
-            {"I", 1},
-            {"IV", 4},
-            {"V", 5},
-            {"IX", 9},
-            {"X", 10},
-            {"XL", 40},
-            {"L", 50},
-            {"XC", 90},
-            {"C", 100},
-            {"CD", 400},
-            {"D", 500},
-            {"CM", 900},
-            {"M", 1000}
-        };
-
-        s = s.ToUpper();
         for (int i = 0; i < s.Length; i++)
         {
-            string newSrt = s[i].ToString();
-            if (i < s.Length - 1)
+            if (i == s.Length - 1)
             {
-                if (s[i].ToString() == "I")
-                {
-                    if (s[i + 1].ToString() == "V" || s[i + 1].ToString() == "X")
-                    {
-                        newSrt += s[i + 1].ToString();
-                        i += 1;
-                    }
-                }
-                else if (s[i].ToString() == "X")
-                {
-                    if (s[i + 1].ToString() == "L" || s[i + 1].ToString() == "C")
-                    {
-                        newSrt += s[i + 1].ToString();
-                        i += 1;
-                    }
-                }
-                else if (s[i].ToString() == "C")
-                {
-                    if (s[i + 1].ToString() == "D" || s[i + 1].ToString() == "M")
-                    {
-                        newSrt += s[i + 1].ToString();
-                        i += 1;
-                    }
-                }
+                convertedInteger += dictionary[s[i]];
+                break;
             }
 
-            foreach (KeyValuePair<string, int> entry in romanToInt)
+            if (dictionary[s[i]] >= dictionary[s[i + 1]])
             {
-                if (entry.Key == newSrt)
-                {
-                    convertedInteger += entry.Value;
-                }
+                convertedInteger += dictionary[s[i]];
+            }
+            else
+            {
+                convertedInteger += dictionary[s[i + 1]] - dictionary[s[i]];
+                i++;
             }
         }
 
         return convertedInteger;
     }
+
+
+    // First approach
+    // readonly Dictionary<string, int> dictionary = new()
+    // {
+    //     {"I", 1},
+    //     {"IV", 4},
+    //     {"V", 5},
+    //     {"IX", 9},
+    //     {"X", 10},
+    //     {"XL", 40},
+    //     {"L", 50},
+    //     {"XC", 90},
+    //     {"C", 100},
+    //     {"CD", 400},
+    //     {"D", 500},
+    //     {"CM", 900},
+    //     {"M", 1000}
+    // };
+    //
+    // public int RomanToInt(string s)
+    // {
+    //     int convertedInteger = 0;
+    //     
+    //     s = s.ToUpper();
+    //     for (int i = 0; i < s.Length; i++)
+    //     {
+    //         string newSrt = s[i].ToString();
+    //         if (i < s.Length - 1)
+    //         {
+    //             if (s[i].ToString() == "I")
+    //             {
+    //                 if (s[i + 1].ToString() == "V" || s[i + 1].ToString() == "X")
+    //                 {
+    //                     newSrt += s[i + 1].ToString();
+    //                     i += 1;
+    //                 }
+    //             }
+    //             else if (s[i].ToString() == "X")
+    //             {
+    //                 if (s[i + 1].ToString() == "L" || s[i + 1].ToString() == "C")
+    //                 {
+    //                     newSrt += s[i + 1].ToString();
+    //                     i += 1;
+    //                 }
+    //             }
+    //             else if (s[i].ToString() == "C")
+    //             {
+    //                 if (s[i + 1].ToString() == "D" || s[i + 1].ToString() == "M")
+    //                 {
+    //                     newSrt += s[i + 1].ToString();
+    //                     i += 1;
+    //                 }
+    //             }
+    //         }
+    //
+    //         convertedInteger += dictionary[newSrt];
+    //     }
+    //
+    //     return convertedInteger;
+    // }
 }
